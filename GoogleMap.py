5a7de1e11f005aed87faedb41791d7111e5837e0
Claus Holmgaard <github@clausnet.dk>
2021-03-16 18:42:28 +0100

    Update GoogleMap.py

--- GoogleMap.py
+++ GoogleMap.py
@@ -1,8 +1,9 @@
 from flask_googlemaps import GoogleMaps, Map, icons
 
 # Hardcoding some locations, to avoid too many google map requests
-EVENT_LOCATION = [56.143218266395216, 8.994740213932541]
-A_MARKER = [56.14352908145836, 8.993624414924469]
+EVENT_LOCATION = [57.4627016845673, 9.983457412129107]
+CHURCH_LOCATION = [57.45037451972904, 9.99648629863845]
+CENTER = [57.45771117023997, 9.987441574012726]
 
 class GMap(object):
     def __init__(self, logger, app, api_key):
@@ -20,8 +21,8 @@ class GMap(object):
         gmap = Map(
             identifier="gmap",
             #varname="gmap",
-            lat=EVENT_LOCATION[0],
-            lng=EVENT_LOCATION[1],
+            lat=CENTER[0],
+            lng=CENTER[1],
             #markers={
             #    icons.dots.green: [(EVENT_LOCATION[0], EVENT_LOCATION[1], 'Main Event!')],
             #    icons.dots.blue: [(A_MARKER[0], A_MARKER[1], 'Relevant Location')]
@@ -31,17 +32,17 @@ class GMap(object):
                     'icon': 'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
                     'lat': EVENT_LOCATION[0],
                     'lng': EVENT_LOCATION[1],
-                    'infobox': '<a target="_blank" href="https://www.google.com/maps/place/Herningsholmvej+7,+7400+Herning/@56.1430569,8.9925086,17z/data=!3m1!4b1!4m5!3m4!1s0x464bbc01922cc8cf:0x2bd3b6a7cc278f7d!8m2!3d56.1430569!4d8.9946972">Yay!</a>'
+                    'infobox': '<a target="_blank" href="https://www.google.com/maps/place/N%C3%B8rregade+22,+9800+Hj%C3%B8rring/@57.4625459,9.9813009,17z/data=!3m1!4b1!4m5!3m4!1s0x4648ce6fdb84f9fd:0xabe5f8efd947f34e!8m2!3d57.4625459!4d9.9834896">Vendelbohus</a>'
                 },
                 {
                     'icon': 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png',
-                    'lat':A_MARKER[0],
-                    'lng': A_MARKER[1],
-                    'infobox': '<b>Hello World from other place</b>'
+                    'lat':CHURCH_LOCATION[0],
+                    'lng': CHURCH_LOCATION[1],
+                    'infobox': '<a target="_blank" href="https://www.google.com/maps/place/Bispensgade+67,+9800+Hj%C3%B8rring/@57.450236,9.9942976,17z/data=!3m1!4b1!4m5!3m4!1s0x4648ce0a9e3b671b:0x291eacc6982e8b2f!8m2!3d57.450236!4d9.9964863">Sankt Maria, martyrernes Dronning</a>'
                 }
             ],
             style=f'height:{set_height}px;width:{set_width}px;',
-            zoom=16,
+            zoom=13.9,
             mapdisplay=True
         )
 
